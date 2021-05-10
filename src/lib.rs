aa088f98d4badcb7acec5205ce1ff22a115f5b26
fredpit <frederic.pitoizet@gmail.com>
2021-05-10 11:21:52 +0200

    more funcs

--- src/lib.rs
+++ src/lib.rs
@@ -60,6 +60,15 @@ impl<const N: usize> ByteString<N> {
         }
     }
 
+    pub fn append_str(&mut self, s: &str) {
+        for b in s.bytes() { self.append(b); }
+    }
+ 
+    pub fn from_str(&mut self, s: &str) {
+        self.clear();
+        self.append_str(s);
+    }
+
     pub fn has_byte(&self, b: u8) -> bool {
         for bb in &self.buf[0..self.pos] {
             if *bb == b { return true }
@@ -83,18 +92,20 @@ impl<const N: usize> ByteString<N> {
     }
 }
 
-impl<const N: usize>Write for ByteString<N> {
+
+impl<const N: usize> Write for ByteString<N> {
     fn write_str(&mut self, s: &str) -> Result<(), core::fmt::Error> {
         for b in s.bytes() { self.append(b); }
         Ok(())
     }
 }
 
-impl<'a, const N: usize>Into<&'a str> for &'a ByteString<N> {
+impl<'a, const N: usize> Into<&'a str> for &'a ByteString<N> {
     fn into(self) -> &'a str {
         unsafe { from_utf8_unchecked(self.slice()) }
     }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -102,6 +113,7 @@ mod tests {
     #[test]
     fn it_works() {
         let mut bs = crate::ByteString::<20>::new();
+        assert_eq!(bs.size(), 20);
         bs.append('A' as u8);
         assert_eq!(bs.len(), 1);
         let _ = write!(bs, "BCD");
@@ -113,6 +125,13 @@ mod tests {
         assert_eq!(slice[1], 66u8);
         assert_eq!(slice[2], 67u8);
         assert_eq!(slice[3], 68u8);
+        bs.del_last();
+        assert_eq!(bs.len(), 3);
+        let st : &str = (&bs).into();
+        assert_eq!(st, "ABC");
+        let _ = write!(bs, "   ");
+        bs.trim_end();
+        assert_eq!(bs.len(), 3);
         let _ = write!(bs, "12345678901234567890");
         assert_eq!(bs.len(), 20);
         bs.clear();
